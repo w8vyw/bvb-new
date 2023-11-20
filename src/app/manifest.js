62d2db48b57cda77f4ee9931ed2831f7889090c2
Your Name01~ <you@example.com>
2023-11-20 21:19:22 +0300

    minor

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,9 +1,8 @@
 export default function manifest() {
 	return {
-		name: 'AllinHouse',
-		short_name: 'AllinHouse',
-		description:
-			'Архитектурное бюро AllinHouse было основано в 2021г и разрабатывает проекты повсей России. В нашем портфолио более 40 проектов частных домов разного класса и стиля.',
+		name: '',
+		short_name: '',
+		description: '',
 		start_url: '/',
 		display: 'standalone',
 		background_color: '#fff',
--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -5,25 +5,30 @@ import { Wrapper } from '@/components/ui/Wrapper/Wrapper'
 
 export function Footer() {
 	return (
-		<footer>
-			<Logo />
-			Оперативная международная доставка грузов
-			<a href='tel:+79857757493'>+7 985 775 74 93</a>
-			<a href='tel:+79604161771'>+7 960 416 17 71</a>
-			<a href='tel:+905511890683'>+90 551 189 06 83</a>
-			<a href='mailto:info@bvblogistics.com'>info@bvblogistics.com</a>
-			<a
-				href='https://yandex.ru/maps/104760/inegol/house/66_sok_7b/ZkAYcg5mQU0EQFpqfX1xd31jZQ'
-				target='_blank'
-			>
-				<p>66-я улица, 7B, Джумхуриет, Инегёль, Бурса, Турция</p>
-			</a>
-			<a
-				href='https://yandex.ru/maps/104760/inegol/house/66_sok_7b/ZkAYcg5mQU0EQFpqfX1xd31jZQ'
-				target='_blank'
-			>
-				<p>CUMHURİYET MAH. 66. SK. NO: 7 J- İÇ KAPI NO: - İNEGÖL/BURSA</p>
-			</a>
+		<footer className={styles.Footer}>
+			<Wrapper className={styles.Wrapper}>
+				<div className={styles.Block}>
+					<Logo />
+					Оперативная международная доставка грузов
+				</div>
+				<div className={styles.Block}>
+					<a href='tel:+79857757493'>+7 985 775 74 93</a>
+					<a href='tel:+79604161771'>+7 960 416 17 71</a>
+					<a href='tel:+905511890683'>+90 551 189 06 83</a>
+					<a href='mailto:info@bvblogistics.com'>info@bvblogistics.com</a>
+				</div>
+				<div className={styles.Block}>
+					<a
+						href='https://yandex.ru/maps/104760/inegol/house/66_sok_7b/ZkAYcg5mQU0EQFpqfX1xd31jZQ'
+						target='_blank'
+					>
+						<span>66-я улица, 7B, Джумхуриет, Инегёль, Бурса, Турция</span>
+						<span>
+							CUMHURİYET MAH. 66. SK. NO: 7 J- İÇ KAPI NO: - İNEGÖL/BURSA
+						</span>
+					</a>
+				</div>
+			</Wrapper>
 		</footer>
 	)
 }
